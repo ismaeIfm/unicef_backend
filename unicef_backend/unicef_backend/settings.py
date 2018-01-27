16cdca5704b36b98a3b1ea9a75d0e847448a72d3
Ismael <ismaelfm_@ciencias.unam.mx>
2018-01-27 13:14:48 -0600

    Adding constants file

---
+++ unicef_backend/unicef_backend/settings.py
@@ -0,0 +1,20 @@
+INDEX = 'dashboard'
+
+RETOS_FLOWS = ['reto']
+INCENTIVOS_FLOWS = ['incentives']
+RECORDATORIOS_FLOWS = ['freePD', 'getBirth', 'miAlerta']
+PLANIFICACION_FLOWS = ['planning', 'miAlerta_followUp', 'miAlta']
+PREOCUPACIONES_FLOWS = ['prevent', 'concerns', 'miscarriage', 'prematuro']
+CONSEJOS_FLOWS = [
+    'consejo', 'mosquitos', 'development', 'lineaMaterna', 'milk', 'nutrition',
+    'extra', 'labor'
+]
+
+MIALERTA_FLOW = "07d56699-9cfb-4dc6-805f-775989ff5b3f"
+MIALERTA_NODE = "response_1"
+CANCEL_FLOW = "dbd5738f-8700-4ece-8b8c-d68b3f4529f7"
+CANCEL_NODE = "response_3"
+
+FIELDS_STATE = "fields.rp_state_number"
+FIELDS_MUN = "fields.rp_mun"
+FIELDS_DELIVERY = "fields.rp_deliverydate"
