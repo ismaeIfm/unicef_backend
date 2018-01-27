cb05beeaf8d5aa27b977644f44ce08b30c9659fa
Ismael <ismaelfm_@ciencias.unam.mx>
2018-01-27 14:12:21 -0600

    Adding example of webargs usage

--- unicef_backend/unicef_backend/views.py
+++ unicef_backend/unicef_backend/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response
 from webargs import fields
 from webargs.flaskparser import use_kwargs
+
+from unicef_backend import contacts
 
 api = Blueprint('api', __name__)
 
@@ -16,10 +18,23 @@ def hello():
     return "Hello World!"
 
 
-@api.route("/pregnant_by_state")
+@api.route("/number_babies_by_mun", methods=['POST'])
 @use_kwargs(mun_args, locations=('json', 'form', 'query'))
-def get_pregnant_by_state(state, start_date, end_date):
-    print(state, start_date, end_date)
+def ge_number_babies_by_mun(state, start_date, end_date):
 
+    return make_response(
+        jsonify({
+            'response': contacts.number_babies_by_mun(state)
+        }), 200)
+
+
+"""
+curl -X POST \
+  http://localhost:5000/number_babies_by_mun \
+  -H 'Cache-Control: no-cache' \
+  -H 'Content-Type: application/json' \
+  -H 'Postman-Token: 5b2faa43-1c46-5047-e6f7-13a0fa99279d' \
+  -d '{"state": "29"}'
+ """
 
 # Anadir consideraciones en fechas dependiendo del campo
