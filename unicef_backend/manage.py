5869a245bfc3d8d7d1472b6124c0c3035ba547af
Ismael <ismaelfm_@ciencias.unam.mx>
2018-01-27 13:19:05 -0600

    refactoring manage.py

--- unicef_backend/manage.py
+++ unicef_backend/manage.py
@@ -10,9 +10,9 @@ from flask_script import Manager
 from temba_client.v2 import TembaClient
 from tqdm import tqdm
 
-from unicef_backend import create_app
+from unicef_backend import create_app, settings
 from unicef_backend.indexes import Action, Contact, Run, Value
-from unicef_backend.utils_els import _format_date, _format_str
+from unicef_backend.utils import _format_date, _format_str
 
 app = create_app('development')
 mx_client = TembaClient('rapidpro.datos.gob.mx', os.getenv('TOKEN_MX'))
@@ -27,49 +27,16 @@ CONTACT_FIELDS = {
     'rp_Mamafechanac': _format_date,
     'rp_duedate': _format_date
 }
-"""
-Parse string date depends on format
-"""
-
-
-def format_date(fields_dic, key):
-    item = fields_dic[key]
-    if item:
-        try:
-            return parse(item).isoformat()
-        except ValueError:
-            pass
-    return None
-
-
-@manager.command
-def delete_index(force=False):
-    index = Index('dashboard')
-    index.delete(ignore=404)
-
-
-@manager.command
-def load_flows(force=False):
-    Action.init()
-    data = json.load(open('actions.json'))
-    for i in tqdm(data, desc='==> Getting Actions'):
-        for x, y in i.items():
-            message = y["base"] if "base" in y else y["spa"]
-            message = message["base"] if "base" in message and type(
-                message) == dict else message
-            Action(**{'action_id': x, 'msg': message, '_id': x}).save()
 
 
 def insert_one_contact(c):
     fields = {k: v(c.fields.get(k, '')) for k, v in CONTACT_FIELDS.items()}
+    groups = [{'uuid': i.uuid, 'name': i.name} for i in c.groups]
     contact = {
         '_id': c.uuid,
         'urns': c.urns,
         'created_on': c.created_on,
-        'groups': [{
-            'uuid': i.uuid,
-            'name': i.name
-        } for i in c.groups],
+        'groups': groups,
         'modified_on': c.modified_on,
         'uuid': c.uuid,
         'name': c.name,
@@ -83,92 +50,67 @@ def insert_one_contact(c):
     return cs
 
 
-@manager.command
-def download_contacts(force=False):
-    Contact.init()
-    #Group.init()
-    Run.init()
-    Value.init()
-    for c in tqdm(
-            mx_client.get_contacts(after=(
-                datetime.utcnow() - timedelta(days=30)).isoformat()).all(),
-            desc='==> Getting Contacts'):
-        #Only save misalud contacts
-        if not "MIGRACION_PD" in [i.name for i in c.groups]:
-            #Normalize date
-            insert_one_contact(c)
-
-
 def search_contact(uuid):
     try:
         contact = Contact.get(id=uuid)
     except NotFoundError:  #Need to update datebase
         contacts = mx_client.get_contacts(uuid=uuid).all()
         if contacts:
-            c = contacts[0]
-            contact = c.serialize()
-            contact = insert_one_contact(c)
+            contact = insert_one_contact(contacts[0])
     return contact
-
-
-def create_base_node(run, contact):
-    if not contact.get('fields'):
-        print(contact)
-    return {
-        'flow_uuid': run.flow.uuid,
-        'flow_name': run.flow.name,
-        'contact_uuid': run.contact.uuid,
-        'rp_deliverydate': contact.get("fields", {}).get("rp_deliverydate"),
-        'rp_duedate': contact.get("fields", {}).get("rp_duedate"),
-        'rp_state_number': contact.get("fields", {}).get("rp_state_number"),
-        'rp_mun': contact.get("fields", {}).get("rp_mun"),
-        'urns': contact["urns"],
-        'rp_atenmed': contact.get("fields", {}).get("rp_atenmed"),
-        'rp_ispregnant': contact.get("fields", {}).get("rp_ispregnant"),
-        'rp_Mamafechanac': contact.get("fields", {}).get("rp_Mamafechanac"),
-        'groups': [i for i in contact.get("groups", [])]
-    }
 
 
 def insert_value_run(run):
     for value in run.values:
 
-        contact = search_contact(run.contact.uuid)
-        value_dic = create_base_node(run, contact.to_dict())
-        value_dic["node"] = value,
-        value_dic["category"] = run.values[value].category
-        value_dic["time"] = run.values[value].time
-        value_dic["response"] = run.values[value].value
-        Value(**value_dic).save()
+        value_dict = {
+            'flow_uuid': run.flow.uuid,
+            'flow_name': run.flow.name,
+            'contact_uuid': run.contact.uuid,
+            'node': value,
+            'category': run.values[value].category,
+            'time': run.values[value].time,
+            'response': run.values[value].value
+        }
+
+        v = Value(**value_dict)
+        v.meta.parent = run.contact.uuid
+        v.save()
 
 
 def get_type_flow(flow_name):
-
-    if any([
-            i in flow_name
-            for i in [
-                'consejo', 'mosquitos', 'development', 'lineaMaterna', 'milk',
-                'nutrition', 'extra', 'labor'
-            ]
-    ]):
+    aux_flow = lambda fs: any([i in flow_name for i in fs])
+    if aux_flow(settings.CONSEJOS_FLOWS):
         return 'consejos'
-    elif any([i in flow_name for i in ['reto']]):
+    elif aux_flow(settings.RETOS_FLOWS):
         return 'retos'
-    elif any(
-        [i in flow_name for i in ['reto', 'freePD', 'getBirth', 'miAlerta']]):
+    elif aux_flow(settings.RECORDATORIOS_FLOWS):
         return 'recordatorios'
-    elif any(
-        [i in flow_name for i in ['planning', 'miAlerta_followUp', 'miAlta']]):
+    elif aux_flow(settings.PLANIFICACION_FLOWS):
         return 'planificacion'
-    elif any([i in flow_name for i in ['incentives']]):
+    elif aux_flow(settings.INCENTIVOS_FLOWS):
         return 'incentivos'
-    elif any([
-            i in flow_name
-            for i in ['prevent', 'concerns', 'miscarriage', 'prematuro']
-    ]):
+    elif aux_flow(settings.PREOCUPACIONES_FLOWS):
         return 'preocupaciones'
     else:
         return 'otros'
+
+
+def insert_run(run, path_item, action):
+
+    run_dict = {
+        'flow_uuid': run.flow.uuid,
+        'flow_name': run.flow.name,
+        'contact_uuid': run.contact.uuid,
+        'type': get_type_flow(run.flow.name),
+        'action_uuid': action['action_id'],
+        'time': path_item.time,
+        'msg': action['msg'],
+    }
+
+    r = Run(**run_dict)
+    r.meta.parent = run.contact.uuid
+    r.save()
 
 
 def update_runs(after=None, last_runs=None):
@@ -176,42 +118,71 @@ def update_runs(after=None, last_runs=None):
         last_runs = mx_client.get_runs(after=after).all(
             retry_on_rate_exceed=True)
     for run in last_runs:
-        if run.flow.uuid == "07d56699-9cfb-4dc6-805f-775989ff5b3f":  #MiAlerta
+        search_contact(run.contact.uuid)
+        if run.flow.uuid == settings.MIALERTA_FLOW:  #MiAlerta
             insert_value_run(run)
-        elif run.flow.uuid == "dbd5738f-8700-4ece-8b8c-d68b3f4529f7":  #Cancela
+        elif run.flow.uuid == settings.CANCEL_FLOW:  #Cancela
             insert_value_run(run)
         for path_item in run.path:
-            #Search action
             try:
-                action = Action.get(id=path_item.node)
+                action = Action.get(id=path_item.node)  # Search action
             except NotFoundError:
                 #We ignore the path item if has a split or a group action
                 continue
-            contact = search_contact(run.contact.uuid)
-            action_dict = create_base_node(run, contact.to_dict())
-            action_dict['time'] = path_item.time
-            action_dict['action_uuid'] = action["action_id"]
-            action_dict['msg'] = action["msg"]
-            action_dict['type'] = get_type_flow(
-                action_dict.get('flow_name', ''))
-            Run(**action_dict).save()
+            insert_run(run, path_item, action)
 
 
 @manager.command
-def preprocess_runs(force=False):
+def load_flows(force=False):
+    Action.init()
+    data = json.load(open('actions.json'))
+    for i in tqdm(data, desc='==> Getting Actions'):
+        for _id, m in i.items():
+            message = m["base"] if "base" in m else m["spa"]
+            message = message["base"] if "base" in message and type(
+                message) == dict else message
+            Action(**{'action_id': _id, 'msg': message, '_id': _id}).save()
+
+
+@manager.command
+def download_contacts(force=False):
+    date = (datetime.utcnow() - timedelta(days=30)).isoformat()
+    contacts = mx_client.get_contacts(after=date).all()
+    for c in tqdm(contacts, desc='==> Getting Contacts'):
+        #Only save misalud contacts
+        if not "MIGRACION_PD" in [i.name for i in c.groups]:
+            #Normalize date
+            insert_one_contact(c)
+
+
+@manager.command
+def delete_index(force=False):
+    index = Index(settings.INDEX)
+    index.delete(ignore=404)
+
+
+@manager.command
+def create_index():
+    index = Index(settings.INDEX)
+    index.delete(ignore=404)
+    for t in [Action, Contact, Run, Value]:
+        index.doc_type(t)
+    index.create()
+
+
+@manager.command
+def download_test_runs(force=False):
     after = datetime.utcnow() - timedelta(days=1)
     after = after.isoformat()
     update_runs(after)
     print("Descargando alerta")
     #Temporal download mialerta runs
-    runs = mx_client.get_runs(
-        flow="07d56699-9cfb-4dc6-805f-775989ff5b3f").all()
+    runs = mx_client.get_runs(flow=settings.MIALERTA_FLOW).all()
     update_runs(last_runs=runs)
 
     print("Descargando cancela")
     #Temporal download cancela runs
-    runs = mx_client.get_runs(
-        flow="dbd5738f-8700-4ece-8b8c-d68b3f4529f7").all()
+    runs = mx_client.get_runs(flow=settings.CANCEL_FLOW).all()
     update_runs(last_runs=runs)
 
 
