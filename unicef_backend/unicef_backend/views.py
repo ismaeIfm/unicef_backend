11a9897e3751c62e0338d3c73c2722d1009d31a1
Your Name <you@example.com>
2018-02-08 18:29:32 -0600

    Add baby age without date filter on views

--- unicef_backend/unicef_backend/views.py
+++ unicef_backend/unicef_backend/views.py
@@ -27,7 +27,7 @@ def configure_deliverydate(start_date=None, end_date=None):
 
 
 def configure_duedate(start_date=None, end_date=None):
-    if start_date:
+    if end_date:
         end_date = end_date + relativedelta(months=9)
     return start_date, end_date
 
@@ -80,13 +80,12 @@ def view_users_by_mom_age(start_date, end_date):
 def view_users_by_baby_age(start_date, end_date):
     """ """
     response = {}
-    lista = contacts.number_contacts_by_baby_age(
-        start_date=start_date, end_date=end_date)
+    lista = contacts.number_contacts_by_baby_age()
     for dictionary in lista:
-        from_key = datetime.strptime(dictionary["from_as_string"],
+        from_key = datetime.strptime(lista[dictionary]["from_as_string"],
                                      "%Y-%m-%dT%H:%M:%S.%fZ")
-        trimester = (datetime.now() - from_key).days / 90
-        response[trimester] = dictionary["doc_count"]
+        trimester = int((datetime.now() - from_key).days / 90)
+        response[trimester] = lista[dictionary]["doc_count"]
 
     return make_response(jsonify({'response': response}), 200)
 
@@ -135,13 +134,6 @@ def view_babies_by_mun(state, start_date, end_date):
     start_date, end_date = configure_deliverydate(start_date, end_date)
     response = contacts.number_babies_by_mun(
         state, start_date=start_date, end_date=end_date)
-    return make_response(jsonify({'response': response}), 200)
-
-
-@api.route("/babies_by_week", methods=['POST', 'GET'])
-def view_babies_by_week():
-    """ """
-    response = contacts.number_babies_by_week()
     return make_response(jsonify({'response': response}), 200)
 
 
@@ -212,9 +204,20 @@ def view_mom_age_by_state(start_date, end_date):
 
 
 @api.route("/baby_age_by_state", methods=['POST', 'GET'])
-def view_baby_age_by_state():
+@use_kwargs(date_args)
+def view_baby_age_by_state(start_date, end_date):
     """ """
-    response = contacts.number_baby_age_by_state()
+    response = {}
+    lista = contacts.number_baby_age_by_state()
+    for dictionary in lista:
+        lista_bucket = dictionary["by_baby_age"]["buckets"]
+        response[dictionary["key"]] = {}
+        for item in lista_bucket:
+            from_key = datetime.strptime(lista_bucket[item]["from_as_string"],
+                                         "%Y-%m-%dT%H:%M:%S.%fZ")
+            trimester = int((datetime.now() - from_key).days / 90)
+            response[dictionary["key"]][trimester] = lista_bucket[item][
+                "doc_count"]
     return make_response(jsonify({'response': response}), 200)
 
 
@@ -243,75 +246,87 @@ def view_channel_by_state(start_date, end_date):
 ##################      Municipios part     ######################
 @api.route("/pregnants_by_mun", methods=['POST', 'GET'])
 @use_kwargs(mun_args)
-def view_pregnants_by_mun(state,start_date, end_date):
+def view_pregnants_by_mun(state, start_date, end_date):
     """
     filter by duedate date
     """
     start_date, end_date = configure_duedate(start_date, end_date)
-    response = contacts.number_pregnant_by_mun(state,
-        start_date=start_date, end_date=end_date)
+    response = contacts.number_pregnant_by_mun(
+        state, start_date=start_date, end_date=end_date)
     return make_response(jsonify({'response': response}), 200)
 
 
 @api.route("/moms_by_mun", methods=['POST', 'GET'])
 @use_kwargs(mun_args)
-def view_moms_by_mun(state,start_date, end_date):
+def view_moms_by_mun(state, start_date, end_date):
     """
     filter by duedate date
     """
     start_date, end_date = configure_duedate(start_date, end_date)
-    response = contacts.number_moms_by_mun(state,
-        start_date=start_date, end_date=end_date)
+    response = contacts.number_moms_by_mun(
+        state, start_date=start_date, end_date=end_date)
     return make_response(jsonify({'response': response}), 200)
 
 
 @api.route("/personal_by_mun", methods=['POST', 'GET'])
 @use_kwargs(mun_args)
-def view_personal_by_mun(state,start_date, end_date):
+def view_personal_by_mun(state, start_date, end_date):
     """
     filter by created_on date
     """
-    response = contacts.number_personal_by_mun(state,
-        start_date=start_date, end_date=end_date)
+    response = contacts.number_personal_by_mun(
+        state, start_date=start_date, end_date=end_date)
     return make_response(jsonify({'response': response}), 200)
 
 
 @api.route("/mom_age_by_mun", methods=['POST', 'GET'])
 @use_kwargs(mun_args)
-def view_mom_age_by_mun(state,start_date, end_date):
+def view_mom_age_by_mun(state, start_date, end_date):
     """ """
-    response = contacts.number_baby_age_by_mun(state,
-        start_date=start_date, end_date=end_date)
+    response = contacts.number_baby_age_by_mun(state)
     return make_response(jsonify({'response': response}), 200)
 
 
 @api.route("/baby_age_by_mun", methods=['POST', 'GET'])
-def view_baby_age_by_mun():
+@use_kwargs(mun_args)
+def view_baby_age_by_mun(state, start_date, end_date):
     """ """
-    response = contacts.number_baby_age_by_state()
+    response = {}
+    lista = contacts.number_baby_age_by_mun(state)
+    for dictionary in lista:
+        lista_bucket = dictionary["by_baby_age"]["buckets"]
+        response[dictionary["key"]] = {}
+        for item in lista_bucket:
+            from_key = datetime.strptime(lista_bucket[item]["from_as_string"],
+                                         "%Y-%m-%dT%H:%M:%S.%fZ")
+            trimester = int((datetime.now() - from_key).days / 90)
+            response[dictionary["key"]][trimester] = lista_bucket[item][
+                "doc_count"]
     return make_response(jsonify({'response': response}), 200)
 
 
 @api.route("/hospitals_by_mun", methods=['POST', 'GET'])
 @use_kwargs(mun_args)
-def view_hospitals_by_mun(state,start_date, end_date):
+def view_hospitals_by_mun(state, start_date, end_date):
     """
     filter by created_on date
     """
-    response = contacts.number_hostpital_by_mun(state,
-        start_date=start_date, end_date=end_date)
+    response = contacts.number_hostpital_by_mun(
+        state, start_date=start_date, end_date=end_date)
     return make_response(jsonify({'response': response}), 200)
 
 
 @api.route("/channel_by_mun", methods=['POST', 'GET'])
 @use_kwargs(mun_args)
-def view_channel_by_mun(state,start_date, end_date):
+def view_channel_by_mun(state, start_date, end_date):
     """
     filter by created_on date
     """
-    response = contacts.number_channel_by_mun(state,
-        start_date=start_date, end_date=end_date)
+    response = contacts.number_channel_by_mun(
+        state, start_date=start_date, end_date=end_date)
+    print(response)
     return make_response(jsonify({'response': response}), 200)
+
 
 """
 curl -X POST \
